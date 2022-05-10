61f5275bd9bf3c0b86602c2ebe5a6e2bed748005
gness1804 <graham.nessler@gmail.com>
2022-05-10 17:14:00 -0500

    refactor: move all code into single file and class

--- simple-cipher.ts
+++ simple-cipher.ts
@@ -1,29 +1,36 @@
-import { generateRandomLowerAlpha } from './utils/generateRandomInt';
-
 export class SimpleCipher {
   key: string;
   valueMapper: Map<string, number>;
+
+  static lowerAlphaRange: number[] = [97, 122]; // char codes for a-z
 
   constructor(key?: string) {
     this.key = key || this.randomize();
     this.valueMapper = this.buildValueMapper();
   }
 
+  private generateRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  private generateRandomLowerAlpha(): string {
+    return String.fromCharCode(this.generateRandomInt(SimpleCipher.lowerAlphaRange[0], SimpleCipher.lowerAlphaRange[1]));
+  }
+
   private randomize(): string {
     let key = '';
     let maxCount = 100;
     while (key.length < maxCount) {
-      key += generateRandomLowerAlpha();
+      key += this.generateRandomLowerAlpha();
     }
     return key;
   }
 
   private buildValueMapper(): Map<string, number> {
     // generates a map with key of lower case letter and value as corresponding shift value. a = 0, b = 1, etc
-    const lowerAlphaRange = [97, 122]; // char codes for a-z
     const map = new Map<string, number>();
     let shiftIndex = 0;
-    let char = lowerAlphaRange[0];
+    let char = SimpleCipher.lowerAlphaRange[0];
     while (shiftIndex < 26) {
       map.set(String.fromCharCode(char), shiftIndex);
       shiftIndex++;
--- utils/generateRandomInt.ts
+++
@@ -1,7 +0,0 @@
-const lowerAlphaRange = [97, 122]; // char codes for a-z
-
-const generateRandomInt = (min: number, max: number): number =>
-  Math.floor(Math.random() * (max - min) + min);
-
-export const generateRandomLowerAlpha = (): string =>
-  String.fromCharCode(generateRandomInt(lowerAlphaRange[0], lowerAlphaRange[1]));
